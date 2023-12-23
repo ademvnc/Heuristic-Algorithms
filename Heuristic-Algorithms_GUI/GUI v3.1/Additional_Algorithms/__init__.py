55bde2b92358555ed615e23a501de83259ef9d6f
ademvnc <ademmavanaci@gmail.com>
2023-12-23 19:08:33 +0300

    Update __init__.py
    __init__.py has updated

--- Heuristic-Algorithms_GUI/GUI v3.1/Additional_Algorithms/__init__.py
+++ Heuristic-Algorithms_GUI/GUI v3.1/Additional_Algorithms/__init__.py
@@ -3,4 +3,15 @@ from .GWO import GWO
 from .HS import harmonySearch
 from .PSO import PSO
 from .SA import SA_geometric, SA_linear
+from .BAT import BAT
+from .CS import CS
+from .DE import DE
+from .FFA import FFA
+from .HHO import HHO
+from .JAYA import JAYA
+from .MFO import MFO
+from .MVO import MVO
+from .SCA import SCA
+from .SSA import SSA
+from .WOA import WOA
 
